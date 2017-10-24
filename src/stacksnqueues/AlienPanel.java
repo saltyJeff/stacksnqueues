959b6d204a578e61f10f0b238d1ccd2f099794dd
18leejefferso <18leejefferso@WHS-F203-15.ad.iusd.org>
2017-10-24 08:16:06 -0700

    added gui stuff

---
+++ src/stacksnqueues/AlienPanel.java
@@ -0,0 +1,7 @@
+package stacksnqueues;
+
+import javax.swing.JPanel;
+
+public class AlienPanel extends JPanel {
+
+}
--- src/stacksnqueues/Main.java
+++ src/stacksnqueues/Main.java
@@ -1,9 +1,18 @@
 package stacksnqueues;
 
-public class Main {
+import javax.swing.*;
 
+public class Main {
+	private static JFrame frame;
+	private static AlienPanel alienPanel;
 	public static void main(String[] args) {
-		
+		frame = new JFrame();
+		alienPanel = new AlienPanel();
+		frame.add(alienPanel);
+		frame.setVisible(true);
+		frame.setTitle("Alien Thing");
+		frame.setSize(640, 480);
+		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
 
 }
